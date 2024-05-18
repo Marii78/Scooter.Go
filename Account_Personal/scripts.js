f9f4c54ff7a626604d06debba565aeaa0fce24de
Marii78 <kurganovammika028@gmail.com>
2024-05-18 08:20:30 +0700

    1

---
+++ Account_Personal/scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('bankCardsButton').addEventListener('click', function() {
+    window.location.href = '../Bank_Cards/cards.html'; 
+});
+
+document.getElementById('logoutButton').addEventListener('click', function() {
+    // Перенаправление на другую страницу
+    window.location.href = "../Login/login.html";
+});
+
+document.getElementById('historiButton').addEventListener('click', function() {
+    // Перенаправление на другую страницу
+    window.location.href = "../History_Booking/history.html";
+});
